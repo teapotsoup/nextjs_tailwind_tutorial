226cd435d3c4da221cfce63f151ca38d717b2e24
teapotsoup <0902ab@naver.com>
2022-06-28 01:59:58 +0900

    컴포넌트 파일 이전과 nextJs의 Image 태그 사용

--- components/indexpack.tsx
+++ components/indexpack.tsx
@@ -1,17 +1,17 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 
 const Indexpack: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-20 flex flex-col space-y-10 min-h-screen">
-      <div className="bg-white p-6 rounded-3xl shadow-xl">
-        <span className="font-semibold text-3xl">Select Item</span>
-
+    <div className="bg-slate-400 xl:place-content-center py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
+      <div className="bg-white dark:bg-gray-500 flex flex-col overflow-hidden rounded-3xl shadow-xl sm:bg-gray-200 sm:hover:bg-gray-400 md:bg-indigo-200 lg:bg-indigo-300 p-6 ">
+        <span className="font-semibold text-3xl text-white">Select Item</span>
         <ul>
           <li>
-            {[1, 2, 3, 4, 5].map((i) => (
+            {[1, 2, 3].map((i) => (
               <div
                 key={i}
-                className="flex justify-between my-2 odd:bg-blue-50 even:bg-yellow-500"
+                className="flex justify-between my-2 odd:bg-blue-50 even:bg-blue-400 rounded-md p-1"
               >
                 <span className="text-gray-500">Grey Chair</span>
                 <span className="font-semibold">$19</span>
@@ -20,8 +20,8 @@ const Indexpack: NextPage = () => {
           </li>
         </ul>
         <ul>
-          {["a", "b", "c", ""].map((item, idx) => (
-            <li className="text-teal-500 py-2 empty:hidden" key={idx}>
+          {["a", "b", ""].map((item, idx) => (
+            <li className="text-neutral-200 py-2 empty:hidden" key={idx}>
               {item}
             </li>
           ))}
@@ -44,28 +44,34 @@ const Indexpack: NextPage = () => {
         </div>
       </div>
       <div className="bg-white overflow-hidden rounded-3xl shadow-xl group">
-        <div className="bg-blue-500 p-6 pb-14">
+        <div className="bg-blue-500 p-6 pb-14 lg:pb-24 xl:pb-40 portrait:bg-indigo-600 landscape:bg-indigo-400">
           <span className="text-white text-2xl">Profile</span>
         </div>
-        <div className="rounded-3xl p-6 bg-white relative -top-5">
+        <div className="dark:bg-gray-500 rounded-3xl p-6 bg-white relative -top-5">
           <div className="flex relative -top-16 items-end justify-between">
             <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-500">Orders</span>
+              <span className="dark:text-white text-sm text-gray-500">
+                Orders
+              </span>
               <span className="font-bold">340</span>
             </div>
             <div className="h-24 w-24 bg-gray-500 rounded-full group-hover:bg-red-300 transition-colors" />
             <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-500">Spent</span>
+              <span className="dark:text-white text-sm text-gray-500">
+                Spent
+              </span>
               <span className="font-bold">$3,140</span>
             </div>
           </div>
           <div className="relative  flex flex-col items-center -mt-10 -mb-5">
-            <span className="text-lg font-bold group-hover:text-red-300 transition-colors">Tony Molloy</span>
+            <span className="text-lg font-bold group-hover:text-red-300 transition-colors">
+              Tony Molloy
+            </span>
             <span className="text-sm text-gray-500">미국</span>
           </div>
         </div>
       </div>
-      <div className="bg-white p-6  overflow-hidden rounded-3xl shadow-xl">
+      <div className="bg-white dark:bg-gray-500 overflow-hidden rounded-3xl shadow-xl p-6 lg:col-span-2 xl:col-span-1">
         <div className="flex mb-5 justify-between items-center">
           <span>◀</span>
           <div className="space-x-3">
@@ -73,11 +79,21 @@ const Indexpack: NextPage = () => {
             <span className="shadow-xl p-2 rounded-md">❤</span>
           </div>
         </div>
-        <div className="bg-zinc-400 h-72 mb-5 rounded-md"></div>
+        <div className="flex justify-between items-center">
+          <Image
+              src="/sinji.png"
+              alt="sinji"
+              layout="intrinsic" // required
+              objectFit="cover" // change to suit your needs
+              className="rounded-md" // just an example
+              width={900} 
+              height={500}
+            />
+        </div>
         <div className="flex flex-col">
           <span className="font-medium text-xl">Swoon Lounge</span>
           <span className="text-xs text-gray-500">Chair</span>
-          <div className="mt-3 mb-5 flex justify-between items-center">
+          <div className="mt-3 mb-5 flex justify-between ">
             <div className="space-x-2">
               <button className="w-5 h-5 rounded-full bg-yellow-500 bg-opacity-50 hover:ring-2 ring-offset-2 ring-yellow-500 transition"></button>
               <button className="w-5 h-5 rounded-full bg-indigo-500  bg-opacity-50 hover:ring-2 ring-offset-2 ring-indigo-500 transition"></button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
 import type { NextPage } from "next";
-import Form from "./form";
-import Indexpack from "./indexpack";
-import Animation from "./animation";
-
+import Indexpack from "../components/indexpack";
+// import Form from "./form";
+// import Indexpack from "../components/indexpack";
+// import Animation from "../components/animation";
 
 const Home: NextPage = () => {
   return (
+    //테일윈드 튜토리얼 페이지
+    <Indexpack></Indexpack>
+    
     //다양한 태그에 애니메이션 적용+중첩 적용
-    <Animation></Animation>
+    // <Animation></Animation>
 
     //폼에 관한 테일윈드 css
     // <Form></Form>
-
-    //테일윈드 튜토리얼 페이지
-    //<Indexpack></Indexpack>
   );
 };
 
